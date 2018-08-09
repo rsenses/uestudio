8e5ffb230542b2fd181820dd5baa23de1920538f
Rubén Silva <ruben.silva@rsenses.com>
2018-08-09 10:48:19 +0200

    deploy

--- deploy.php
+++ deploy.php
@@ -38,12 +38,6 @@ host('ams3.uecluster.com')
     ->stage('production')
     ->set('deploy_path', '/var/www/admin.uecluster.com');
 
-host('nl1.uecluster.com')
-    ->user('root')
-    // ->forwardAgent()
-    ->stage('production')
-    ->set('deploy_path', '/var/www/admin.uecluster.com');
-
 // Specify the repository from which to download your project's code.
 // The server needs to have git installed for this to work.
 // If you're not using a forward agent, then the server has to be able to clone
